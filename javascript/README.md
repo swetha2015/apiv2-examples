bf44faad87d7fe2a875cac6809df2e1f4a624501
Aloys Augustin <aloys.augustin@polytechnique.org>
2016-04-18 18:51:11 +0200

    Implemented scroll function and added 2nd example

--- javascript/README.md
+++ javascript/README.md
@@ -5,11 +5,14 @@ Usage
 -----
 
 1. Install node.js and npm
-Follow the steps [https://docs.npmjs.com/getting-started/installing-node](here) if npm is not installed on your machine.
+
+Follow the steps [here](https://docs.npmjs.com/getting-started/installing-node) if npm is not installed on your machine.
 
 2. Start the test server
+
 Start the example server on your machine by running `npm start` in this directory.
 
 3. Open the example page
+
 Navigate to [http://localhost:8000/](http://localhost:8000) with your browser and play with the examples.
 
--- javascript/app.js
+++ javascript/app.js
@@ -46,7 +46,9 @@ app.service('ScalrAPI', ['$http', function($http) {
     var timestamp = new Date().toISOString();
     var scalrAddress = this.apiSettings.apiUrl;
 
-    if (typeof params === 'string') {
+    if (!params) {
+      queryString = '';
+    } else if (typeof params === 'string') {
       queryString = params; //Assuming the parameters are sorted if they are passed as a string
     } else {
       queryString = this.makeQueryString(params);
@@ -81,15 +83,30 @@ app.service('ScalrAPI', ['$http', function($http) {
     this.makeApiCall('PATCH', path, '', body, onSuccess, onError);
   }
 
-  // Note: with this function the body of the response object passed to the onSuccess
-  // callback will be the aggregated data of all the API calls
+  // Note: with this function the response object passed to the onSuccess will be the one returned by the last API call
+  // of the scroll, to which a new property all_data is added, containing the parsed representation of all the results.
   this.scroll = function(path, params, onSuccess, onError) {
-    this.makeApiCall('GET', path, params, '', function(response) {
-
-    }, onError);
+    this.makeApiCall('GET', path, params, '', 
+      this.makeOnScrollSuccess(path, onSuccess, onError, [], this),
+      onError);
   }
 
-  
+  this.makeOnScrollSuccess = function(path, onSuccess, onError, previousData, that) {
+    return function(response) {
+      var result = response.data;
+      var data = result.data.concat(previousData);
+
+      if (result.pagination.next) {
+        var nextParams = result.pagination.next.substring(result.pagination.next.indexOf('?') + 1);
+        that.makeApiCall('GET', path, nextParams, '', 
+          that.makeOnScrollSuccess(path, onSuccess, onError, data, that),
+          onError);
+      } else {
+        response.all_data = data;
+        onSuccess(response);
+      }
+    };
+  }
 
 }]);
 
@@ -136,15 +153,58 @@ app.controller('APIRequestForm', ['ScalrAPI', '$scope', '$location', '$http', '$
       $scope.result1.body = 'Error: specify a valid environment ID.\nAn environment ID contains only numbers.';
       return;
     }
-    ScalrAPI.fetch('/api/v1beta0/user/' + $scope.params1.envID + '/farms/', {},
+    var path = '/api/v1beta0/user/' + $scope.params1.envID + '/farms/';
+    $scope.result1.statusCode = '';
+    $scope.result1.statusText = 'Fetching ' + path + '...';
+    $scope.result1.body = '';
+    ScalrAPI.scroll(path, {},
       function(response) {
         $scope.result1.statusCode = response.status;
         $scope.result1.statusText = response.statusText;
-        $scope.result1.body = vkbeautify.json(response.data);
+        $scope.result1.body = vkbeautify.json(response.all_data);
       }, function(response) {
         $scope.result1.statusCode = response.status;
         $scope.result1.statusText = response.statusText;
         $scope.result1.body = vkbeautify.json(response.data);
+      });
+  }
+
+  //Example 2: list images in an environment, with filters
+  $scope.params2 = {
+    envID: '',
+    name: '',
+    os: '',
+    cloudPlatform: ''
+  };
+  $scope.result2 = {};
+  $scope.listImages = function() {
+    if (!/^[0-9]+$/.test($scope.params2.envID)) {
+      $scope.result2.body = 'Error: specify a valid environment ID.\nAn environment ID contains only numbers.';
+      return;
+    }
+    queryParameters = {};
+    if ($scope.params2.name) {
+      queryParameters.name = $scope.params2.name;
+    }
+    if ($scope.params2.os) {
+      queryParameters.os = $scope.params2.os;
+    }
+    if ($scope.params2.cloudPlatform) {
+      queryParameters.cloudPlatform = $scope.params2.cloudPlatform;
+    }
+    var path = '/api/v1beta0/user/' + $scope.params2.envID + '/images/';
+    $scope.result2.statusCode = '';
+    $scope.result2.statusText = 'Fetching ' + path + '...';
+    $scope.result2.body = '';
+    ScalrAPI.scroll(path, queryParameters,
+      function(response) {
+        $scope.result2.statusCode = response.status;
+        $scope.result2.statusText = response.statusText;
+        $scope.result2.body = vkbeautify.json(response.all_data);
+      }, function(response) {
+        $scope.result2.statusCode = response.status;
+        $scope.result2.statusText = response.statusText;
+        $scope.result2.body = vkbeautify.json(response.data);
       });
   }
 
